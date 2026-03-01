9ade0860c71d2bb62e1067ae74f661a573b26150
aleex-le-dev <jalias59@outlook.fr>
2026-03-01 20:42:16 +0100

    fix: Corrige le chemin d'exportation des mots de passe Chrome dans le script principal et supprime plusieurs fichiers de test.

--- patcher_clean.ps1
+++
@@ -1,7 +0,0 @@
-$c = Get-Content 'payload_temp.ps1' -Raw
-$c = $c -replace '(?s)"Export".*?"Import"', '"Export" { } "Import"'
-$c = $c -replace '\$arrExp \| Format-Table', ('$arrExp | Export-Csv -Path "' + $HOME + '\Desktop\Chrome_Local_Passwords.csv" -NoTypeInformation; Write-Host "`n[SUCCES] Mots de passe exportes sur le Bureau sous Chrome_Local_Passwords.csv !" -ForegroundColor Green')
-$c = $c -replace '#requires -version 7', ''
-$c = $c -replace '\[System\.Convert\]::FromHexString\(''(.*?)''\)', '($null) # $1'
-$c = $c -replace 'System\.Security\.Cryptography', 'System.Security.Crypto''graphy'
-Set-Content "test_run_chrome.ps1" -Value $c
